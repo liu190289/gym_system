4bc4fb52c37aaae312b7388d02278f869d491740
liuyunning <liuyunning123@hotmail.com>
2025-12-03 02:16:56 +0800

    gym_system

--- groupwork/src/Main.java
+++ groupwork/src/Main.java
@@ -5,6 +5,6 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        DBUtil.testConnection();
+        DBUtil.getConnection();
     }
 }
